67eedc9ffcbb3db5b74a2b58945dbdfbf007696a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-15 21:42:27 +0300

    Create HTTP => #SERVER

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  console.log("Server is Now runing");
+
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1> Welcome to the bicycle shop </h2>");
 });
