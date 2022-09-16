d64e71caf5a4616aeadea9622cd90c81464cfb50
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-16 09:47:14 +0300

    Declaring Routes adn Rendering HTML

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const http = require("http");
-const path = require("path");
+const fs = require("fs").promises;
 
 const server = http.createServer(async (req, res) => {
   if (req.url === "/favicon.ico") return;
 
-  //console.log(req.headers);
   const myURL = new URL(req.url, `http://${req.headers.host}/`);
   const pathname = myURL.pathname;
   const id = myURL.searchParams.get("id");
-  console.log(pathname, id);
-  // console.log(myURL);
 
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1> Welcome to  the bicycle shop </h1>");
+  if (pathname === "/") {
+    const html = await fs.readFile("./view/bicycles.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else if (pathname === "/bicycle" && 5 >= id >= 0) {
+    const html = await fs.readFile("./view/overview.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<div> <h1> File Not Found </h1> </div>");
+  }
 });
 
 server.listen(3000);
