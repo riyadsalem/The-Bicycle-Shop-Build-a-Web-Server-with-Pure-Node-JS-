7846e52a86a0254fbb9fceb1dc3b43a3fd3a1262
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-16 08:39:48 +0300

    parsing URL and Query String

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require("http");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  console.log("Server is Now runing");
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/favicon.ico") return;
+
+  //console.log(req.headers);
+  const myURL = new URL(req.url, `http://${req.headers.host}/`);
+  const pathname = myURL.pathname;
+  const id = myURL.searchParams.get("id");
+  console.log(pathname, id);
+  // console.log(myURL);
 
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1> Welcome to the bicycle shop </h2>");
+  res.end("<h1> Welcome to  the bicycle shop </h1>");
 });
-server.listen(30000);
+
+server.listen(3000);
