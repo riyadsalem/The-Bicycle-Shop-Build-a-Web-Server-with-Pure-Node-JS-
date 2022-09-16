f6af060a52dbd7419ae2fe17704d4431b3942313
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-16 19:11:37 +0300

    Rendering Dynamic Data for Homepage Part1

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ const server = http.createServer(async (req, res) => {
 
   if (pathname === "/") {
     let html = await fs.readFile("./view/bicycles.html", "utf-8");
-    const AllMainBicycles = await fs.readFile("./view/main/bmain.html");
+    const AllMainBicycles = await fs.readFile(
+      "./view/main/bmain.html",
+      "utf-8"
+    );
 
     let allTheBicycles = "";
     for (let index = 0; index < 6; index++) {
-      allTheBicycles += AllMainBicycles;
+      allTheBicycles += resplaceTemplate(AllMainBicycles, bicycles[index]);
     }
 
     html = html.replace(/<%AllMainBicycles%>/g, allTheBicycles);
@@ -28,16 +31,7 @@ const server = http.createServer(async (req, res) => {
     let html = await fs.readFile("./view/overview.html", "utf-8");
     const bicycle = bicycles.find((b) => b.id === id);
 
-    html = html.replace(/<%IMAGE%>/g, bicycle.image);
-    html = html.replace(/<%NAME%>/g, bicycle.name);
-
-    let price = bicycle.originalPrice;
-    if (bicycle.hasDiscount) {
-      price = (price * (100 - bicycle.discount)) / 100;
-      html = html.replace(/<%NEWPRICE%>/g, `$${price}`);
-    } else if (!bicycle.hasDiscount) {
-      html = html.replace(/<%NEWPRICE%>/g, `$0`);
-    }
+    html = resplaceTemplate(html, bicycle);
 
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
@@ -71,3 +65,20 @@ const server = http.createServer(async (req, res) => {
 });
 
 server.listen(3000);
+
+function resplaceTemplate(html, bicycle) {
+  html = html.replace(/<%IMAGE%>/g, bicycle.image);
+  html = html.replace(/<%NAME%>/g, bicycle.name);
+
+  let price = bicycle.originalPrice;
+  if (bicycle.hasDiscount) {
+    price = (price * (100 - bicycle.discount)) / 100;
+    html = html.replace(/<%NEWPRICE%>/g, `$${price}.00`);
+  } else if (!bicycle.hasDiscount) {
+    html = html.replace(/<%NEWPRICE%>/g, `${price}.00`);
+  }
+
+  html = html.replace(/<%OLDPRICE%>/g, `${bicycle.originalPrice}.00`);
+
+  return html;
+}
