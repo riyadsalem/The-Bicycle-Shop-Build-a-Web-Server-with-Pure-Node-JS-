3ba690a62c11cb8183322c00432edb50e8fcc76e
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-16 10:37:50 +0300

    Rendering CSS, Images, SVG Files

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const server = http.createServer(async (req, res) => {
   const myURL = new URL(req.url, `http://${req.headers.host}/`);
   const pathname = myURL.pathname;
   const id = myURL.searchParams.get("id");
+  console.log(req.url);
 
   if (pathname === "/") {
     const html = await fs.readFile("./view/bicycles.html", "utf-8");
@@ -16,6 +17,18 @@ const server = http.createServer(async (req, res) => {
     const html = await fs.readFile("./view/overview.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
+  } else if (/\.(png)$/i.test(req.url)) {
+    const image = await fs.readFile(`./public/image/${req.url.slice(1)}`);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    res.end(image);
+  } else if (/\.(css)$/i.test(req.url)) {
+    const css = await fs.readFile(`./public/css/index.css`);
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(css);
+  } else if (/\.(svg)$/i.test(req.url)) {
+    const svg = await fs.readFile(`./public/image/icons.svg`);
+    res.writeHead(200, { "Content-Type": "image/svg+xml" });
+    res.end(svg);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<div> <h1> File Not Found </h1> </div>");
