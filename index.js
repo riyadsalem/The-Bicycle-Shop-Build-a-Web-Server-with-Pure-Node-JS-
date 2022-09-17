c33eb8f90f1b569a355198ff7db654903343c30a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-17 16:45:24 +0300

    Server setup with Declaring al the Routes with Express Part 2

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const bicycles = require("./data/data.json");
 
 app.listen(3000, (req, res) => {
   console.log("SERVER is running at port 3000");
 });
 
 app.get("/", (req, res, next) => {
-  return res.send("Hello RIYAD");
+  return res.send("HOMEPAGE");
+});
+
+app.get("/bicycle", (req, res) => {
+  const id = req.query.id;
+  const bicycle = bicycles.find((b) => b.id === id);
+  return res.send(bicycle);
 });
