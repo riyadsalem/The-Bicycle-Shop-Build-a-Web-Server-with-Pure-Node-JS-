21c08c662a081a0fa30f2a2ed5392dd2bd97b923
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-17 13:20:51 +0300

    Processing POST Request with Body Parser

--- express.js
+++ express.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express(); // output => Cannot GET /
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("<h1> YAA </h1>");
 });
@@ -10,6 +13,11 @@ app.get("/about", (req, res) => {
   res.json({
     name: "RIYAD",
   });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body.email, " && ", req.body.password);
+  res.send("User Login Successfuly");
 });
 
 app.listen(3000, () => console.log("SERVER IS RUNING AT PORT 3000"));
