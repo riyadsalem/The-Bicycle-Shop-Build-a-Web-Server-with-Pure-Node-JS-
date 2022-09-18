8c02b6ae7ff150fb126afc455b5b8ecf9db3277c
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-18 20:12:40 +0300

    ExpressJs SETUP

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ app.listen(3000, (req, res) => {
   console.log("SERVER is running at port 3000");
 });
 
+/*
+app.use((req, res, next) => {
+  console.log(req.get("host"));
+  next();
+});
+*/
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res, next) => {
-  return res.send("HOMEPAGE");
+  // return res.send("HOMEPAGE");
+  return res.render("bicycles");
 });
 
 app.get("/bicycle", (req, res) => {
   const id = req.query.id;
   const bicycle = bicycles.find((b) => b.id === id);
-  return res.send(bicycle);
+  // return res.send(bicycle);
+  return res.render("overview");
 });
